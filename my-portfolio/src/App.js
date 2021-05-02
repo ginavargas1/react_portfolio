c792e190e6cbd59a5bbbc677b8a0b17cc82f8dae
Gina Vargas <gina.vargas89@outlook.com>
2021-05-01 17:15:44 -0700

    organizing sections

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 
 function App() {
-  return <Portfolio />;
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Portfolio />
+      <Projects />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
