ce7bb6671a5e087adfd9ff8b6a86d36359317cbc
Gina Vargas <gina.vargas89@outlook.com>
2021-05-01 20:38:54 -0700

    fixed footer

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { Button } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css'
 
 function Footer() {
   return (
     <footer className="container" id="additional-links">
        <h4 className="display-6 text-center">Additional Links</h4>
        <div id="footer-links">
-           <button data-target="#linkedin-info">
-              <a href="https://www.linkedin.com/in/ginavargas89/" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a></button>
-           <button data-target="#github-info">
-              <a href="https://github.com/ginavargas1" rel="noopener noreferrer"><i className="fab fa-github"></i></a></button>
+           <Button circular color="linkedin" icon="linkedin" as="a" href="https://www.linkedin.com/in/ginavargas89/" />
+              
+           <Button circular color="grey" icon="github" as="a" href="https://github.com/ginavargas1" />
        </div>
+       <br/>
      </footer>
   );
 }
